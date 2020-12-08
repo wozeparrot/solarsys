e3d62f1e418fb46d6fd7297697f7cab58aa03b8e
Woze Parrot <wozeparrot@gmail.com>
2020-12-07 22:04:34 -0500

    woztop config

---
+++ host/configuration.nix
@@ -0,0 +1,86 @@
+{ config, pkgs, mpkgs, ... }:
+{
+  networking.hostName = "woztop";
+
+  imports = [
+    ../common/profiles/graphical.nix
+    ../common/profiles/laptop.nix
+  ];
+
+  boot.kernelParams = [ "intel_iommu=on" ];
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.plymouth.enable = true;
+
+  hardware.opengl = {
+    extraPackages32 = with pkgs.pkgsi686Linux; [ vaapiIntel ];
+    extraPackages = with pkgs; [
+      vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+      intel-media-driver
+      rocm-opencl-icd
+      rocm-opencl-runtime
+    ];
+  };
+
+  networking.interfaces.enp2s0.useDHCP = true;
+  networking.interfaces.wlp3s0.useDHCP = true;
+
+  environment.systemPackages = with pkgs; [
+    virt-manager
+    virt-viewer
+
+    corectrl
+  ];
+
+  programs.nm-applet.enable = true;
+
+  services.flatpak.enable = true;
+
+  services.xserver.videoDrivers = [ "amdgpu" "modesetting" ];
+  services.xserver.displayManager = {
+    autoLogin = {
+      enable = true;
+      user = "woze";
+    };
+    lightdm.enable = true;
+    session = [
+      {
+        manage = "window";
+        name = "home-manager";
+        start = ''
+          ${pkgs.runtimeShell} $HOME/.hm-xsession &
+          waitPID=$!
+        ''
+      }
+    ];
+  };
+
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="intel_backlight", MODE="0666", RUN+="${pkgs.coreutils}/bin/chmod a+w /sys/class/backlight/%k/brightness"
+  '';
+
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemuOvmf = true;
+      onBoot = "ignore";
+      onShutdown = "shutdown";
+    };
+    kvmgt = {
+      enable = true;
+      vgpus = {
+        "i915-GVTg_V5_4" = {
+          uuid = [ "eb1ec6dc-133e-11eb-a7a0-9714878a69bc" ];
+        };
+      };
+    };
+
+    docker.enable = true;
+  };
+
+  users.users.woze.extraGroups = [ "docker" "libvirtd" ];
+};
---
+++ host/home.nix
@@ -0,0 +1,109 @@
+{ config, pkgs, mpkgs, ... }:
+
+{
+  imports = [
+    ../common/home.nix
+  ];
+
+  # Packages
+  home.packages = with pkgs; [
+    lxappearance
+
+    ss-rofi
+    herbstluftwm
+    i3lock-color
+    pcmanfm
+    polybarFull
+
+    pypy3
+    python3
+    python3Packages.python-language-server
+  ];
+
+  programs.obs-studio = {
+    enable = true;
+    plugins = [ pkgs.obs-v4l2sink ];
+  };
+
+  programs.autorandr = {
+    enable = true;
+    hooks.postswitch = { "update" = "$HOME/scripts/monitor.sh"; };
+    profiles = {
+      "undocked" = {
+        fingerprint = {
+          eDP-1 =
+            "00ffffffffffff0006afed380000000000190104952213780251259358578f281f505400000001010101010101010101010101010101783780b470382e406c30aa0058c11000001a602c80b470382e406c30aa0058c11000001a000000fe003238483830804231353648544e000000000000410296001000000a010a20200009";
+        };
+        config = {
+          eDP-1 = {
+            enable = true;
+            crtc = 0;
+            primary = true;
+            mode = "1920x1080";
+            position = "0x0";
+            rate = "60.05";
+          };
+        };
+      };
+      "docked" = {
+        fingerprint = {
+          eDP-1 =
+            "00ffffffffffff0006afed380000000000190104952213780251259358578f281f505400000001010101010101010101010101010101783780b470382e406c30aa0058c11000001a602c80b470382e406c30aa0058c11000001a000000fe003238483830804231353648544e000000000000410296001000000a010a20200009";
+          DP-1 =
+            "00ffffffffffff004c2de8083242565a2d160104a5341d78222cc1a45650a1280f5054bfef80714f81c0810081809500a9c0b3000101023a801871382d40582c450009252100001e000000fd00384b1e5111000a202020202020000000fc00533234423234300a2020202020000000ff0048544e434230303938340a202000ac";
+        };
+        config = {
+          eDP-1 = {
+            enable = true;
+            crtc = 1;
+            primary = false;
+            mode = "1920x1080";
+            position = "1920x0";
+            rate = "60.05";
+          };
+          DP-1 = {
+            enable = true;
+            crtc = 0;
+            primary = true;
+            mode = "1920x1080";
+            position = "0x0";
+            rate = "60.00";
+          };
+        };
+      };
+    };
+  };
+
+  # Services
+  services.picom = {
+    enable = true;
+    blur = true;
+    blurExclude = [ "class_g = 'slop'" "class_g = 'discord_overlay.py'" ];
+    package = pkgs.ss-picom;
+
+    extraOptions = ''
+      blur: {
+        method = "dual_kawase";
+        strength = 10;
+        background = false;
+        background-frame = false;
+        background-fixed = false;
+      }
+    '';
+  };
+
+  services.kdeconnect.indicator = true;
+  services.pulseeffects.enable = true;
+  services.nextcloud-client.enable = true;
+
+  xsession.enable = true;
+  xsession.windowManager.command = "${pkgs.herbstluftwm}/bin/herbstluftwm";
+  xsession.initExtra = ''
+    sxhkd &
+    dunst &
+    $HOME/.config/polybar/launch.sh
+    feh --bg-fill $HOME/pictures/wallpapers/984194.jpg
+    autorandr -c 
+  '';
+  xsession.scriptPath = ".hm-xsession";
+}
