69d1ce5706141f63954202c1583b857a23e9bdb8
Woze Parrot <wozeparrot@gmail.com>
2020-12-08 22:03:40 -0500

    wlan0 dhcp

--- host/configuration.nix
+++ host/configuration.nix
@@ -29,6 +29,7 @@
 
   networking.interfaces.enp2s0.useDHCP = true;
   networking.interfaces.wlp3s0.useDHCP = true;
+  networking.interfaces.wlan0.useDHCP = true;
 
   environment.systemPackages = with pkgs; [
     virt-manager
