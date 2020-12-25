9729b3af3b3c86e0130fcb7e80d4f732a82d1b51
Woze Parrot <wozeparrot@gmail.com>
2020-12-25 11:55:56 -0500

    remove some packages because xfce

--- host/home.nix
+++ host/home.nix
@@ -7,15 +7,11 @@
 
   # Packages
   home.packages = with pkgs; [
-    lxappearance
     ss.rofi
     herbstluftwm
     i3lock-color
-    pcmanfm
     polybarFull
-    sxhkd
     dunst
-    xfce.exo
 
     pypy3
     python3
@@ -49,55 +45,6 @@
   programs.obs-studio = {
     enable = true;
     plugins = [ pkgs.obs-v4l2sink ];
-  };
-
-  programs.autorandr = {
-    enable = true;
-    hooks.postswitch = { "update" = "$HOME/scripts/monitor.sh"; };
-    profiles = {
-      "undocked" = {
-        fingerprint = {
-          eDP-1 =
-            "00ffffffffffff0006afed380000000000190104952213780251259358578f281f505400000001010101010101010101010101010101783780b470382e406c30aa0058c11000001a602c80b470382e406c30aa0058c11000001a000000fe003238483830804231353648544e000000000000410296001000000a010a20200009";
-        };
-        config = {
-          eDP-1 = {
-            enable = true;
-            crtc = 0;
-            primary = true;
-            mode = "1920x1080";
-            position = "0x0";
-            rate = "60.05";
-          };
-        };
-      };
-      "docked" = {
-        fingerprint = {
-          eDP-1 =
-            "00ffffffffffff0006afed380000000000190104952213780251259358578f281f505400000001010101010101010101010101010101783780b470382e406c30aa0058c11000001a602c80b470382e406c30aa0058c11000001a000000fe003238483830804231353648544e000000000000410296001000000a010a20200009";
-          DP-1 =
-            "00ffffffffffff004c2de8083242565a2d160104a5341d78222cc1a45650a1280f5054bfef80714f81c0810081809500a9c0b3000101023a801871382d40582c450009252100001e000000fd00384b1e5111000a202020202020000000fc00533234423234300a2020202020000000ff0048544e434230303938340a202000ac";
-        };
-        config = {
-          eDP-1 = {
-            enable = true;
-            crtc = 1;
-            primary = false;
-            mode = "1920x1080";
-            position = "1920x0";
-            rate = "60.05";
-          };
-          DP-1 = {
-            enable = true;
-            crtc = 0;
-            primary = true;
-            mode = "1920x1080";
-            position = "0x0";
-            rate = "60.00";
-          };
-        };
-      };
-    };
   };
 
   # Services
