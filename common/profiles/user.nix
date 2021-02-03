a78736857022b5b93970f656fc37315a2f46d5c4
Woze Parrot <wozeparrot@gmail.com>
2021-02-03 08:57:26 -0500

    add to audio group

--- common/profiles/user.nix
+++ common/profiles/user.nix
@@ -5,7 +5,7 @@
   users.users.woze = {
     isNormalUser = true;
     hashedPassword = "$6$UViRjZMnBsCT7$U6tbj1.hFnQgxnN6pm5yaF2AiXsuVVxUGpBC3kMpDLyrUXOnocdxatx.Ffmalu8IzhhSA/i2EjpvlIOgSLLJS0";
-    extraGroups = [ "wheel" "video" "input" "plugdev" ];
+    extraGroups = [ "wheel" "video" "input" "plugdev" "audio" ];
     shell = pkgs.fish;
   };
 }
