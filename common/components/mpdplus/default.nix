c83d87710d59fed4da52c5abbbc7f6be01657c8f
Woze Parrot <wozeparrot@gmail.com>
2021-05-18 16:11:42 -0400

    ncmpcpp

--- common/components/mpdplus/default.nix
+++ common/components/mpdplus/default.nix
@@ -11,9 +11,9 @@
 
       audio_output {
         type "fifo"
-        name "my_fifo"
+        name "Visualizer feed"
         path "/tmp/mpd.fifo"
-        format "44100:16:2"
+        format "44100:16:1"
       }
 
       replaygain track
@@ -31,12 +31,13 @@
     enable = true;
     package = pkgs.ncmpcpp.override { visualizerSupport = true; };
     settings = {
-      visualizer_fifo_path = "/tmp/mpd.fifo";
-      visualizer_output_name = "my_fifo";
-      visualizer_sync_interval = "12";
+      visualizer_data_source = "/tmp/mpd.fifo";
+      visualizer_output_name = "Visualizer feed";
+      visualizer_sync_interval = "0";
       visualizer_in_stereo = "no";
       visualizer_type = "spectrum";
-      visualizer_look = "||";
+      visualizer_fps = "60";
+      visualizer_spectrum_smooth_look = "yes"
       visualizer_color = "green";
 
       user_interface = "alternative";
