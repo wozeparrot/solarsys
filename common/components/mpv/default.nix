5acf42058237d05acd9b068c17f06e23c07469c8
Woze Parrot <wozeparrot@gmail.com>
2021-05-22 23:11:20 -0400

    testing scripts

--- common/components/mpv/default.nix
+++ common/components/mpv/default.nix
@@ -2,17 +2,17 @@
 {
   programs.mpv = {
     enable = true;
-    package = pkgs.wrapMpv (pkgs.mpv-unwrapped.override { vapoursynthSupport = true; }) { youtubeSupport = true; };
+    package = pkgs.wrapMpv (pkgs.mpv-unwrapped.override { vapoursynthSupport = true; }) { youtubeSupport = true; scripts = with pkgs.mpvScripts; [
+      mpris
+      autoload
+      thumbnail
+    ]; };
     config = {
       force-window = true;
       ytdl-format = "bestvideo+bestaudio";
       cache-default = 4000000;
     };
     defaultProfiles = [ "gpu-hq" ];
-    scripts = with pkgs.mpvScripts; [
-      mpris
-      autoload
-      thumbnail
-    ];
+    
   };
 }
