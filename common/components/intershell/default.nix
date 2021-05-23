bc9f3518221c590d1747137cc7c8636ad702932b
Woze Parrot <wozeparrot@gmail.com>
2021-05-23 12:20:51 -0400

    color scheme fish

--- common/components/intershell/default.nix
+++ common/components/intershell/default.nix
@@ -3,33 +3,31 @@
 let
   fishConfig = ''
     # color scheme
-    set -u fish_color_autosuggestion 875f87
-    set -u fish_color_cancel normal
-    set -u fish_color_command 5f5fff
-    set -u fish_color_comment 990000
-    set -u fish_color_cwd 008000
-    set -u fish_color_cwd_root 800000
-    set -u fish_color_end 009900
-    set -u fish_color_error ff0000
-    set -u fish_color_escape 00a6b2
-    set -u fish_color_history_current normal
-    set -u fish_color_host normal
-    set -u fish_color_host_remote yellow
-    set -u fish_color_match normal
-    set -u fish_color_normal normal
-    set -u fish_color_operator 00a6b2
-    set -u fish_color_param d700ff
-    set -u fish_color_quote d7005f
-    set -u fish_color_redirection 00afff
-    set -u fish_color_search_match ffff00
-    set -u fish_color_selection c0c0c0
-    set -u fish_color_status red
-    set -u fish_color_user 00ff00
-    set -u fish_color_valid_path normal
-    set -u fish_pager_color_completion normal
-    set -u fish_pager_color_description B3A06D\x1eyellow
-    set -u fish_pager_color_prefix white\x1e\x2d\x2dbold\x1e\x2d\x2dunderline
-    set -u fish_pager_color_progress brwhite\x1e\x2d\x2dbackground\x3dcyan
+    set -U fish_color_normal normal
+    set -U fish_color_command c397d8
+    set -U fish_color_quote b9ca4a
+    set -U fish_color_redirection 70c0b1
+    set -U fish_color_end c397d8
+    set -U fish_color_error d54e53
+    set -U fish_color_param 7aa6da
+    set -U fish_color_selection white --bold --background=brblack
+    set -U fish_color_search_match bryellow --background=brblack
+    set -U fish_color_history_current --bold
+    set -U fish_color_operator 00a6b2
+    set -U fish_color_escape 00a6b2
+    set -U fish_color_cwd green
+    set -U fish_color_cwd_root red
+    set -U fish_color_valid_path --underline
+    set -U fish_color_autosuggestion 969896
+    set -U fish_color_user brgreen
+    set -U fish_color_host normal
+    set -U fish_color_cancel -r
+    set -U fish_pager_color_completion normal
+    set -U fish_pager_color_description B3A06D yellow
+    set -U fish_pager_color_prefix white --bold --underline
+    set -U fish_pager_color_progress brwhite --background=cyan
+    set -U fish_color_match --background=brblue
+    set -U fish_color_comment e7c547
 
     set -x GOPATH ~/local/Go
     set -x HISTSIZE 20000
