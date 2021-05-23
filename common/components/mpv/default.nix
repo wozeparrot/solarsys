22a1d2c96a8a97f9d4c762ae56d63f3ab5af95cd
Woze Parrot <wozeparrot@gmail.com>
2021-05-23 10:46:57 -0400

    mpv config

--- common/components/mpv/default.nix
+++ common/components/mpv/default.nix
@@ -8,10 +8,29 @@
     };
     config = {
       force-window = true;
-      ytdl-format = "bestvideo+bestaudio";
+      ytdl-format = "bestvideo[height<=?1080]+bestaudio";
       save-position-on-quit = true;
       osc = false;
+
+      hwdec = "auto-copy";
+      hwdec-codecs = "all";
+      hr-seek-framedrop = false;
+
+      profile = "gpu-hq";
+      gpu-api = "vulkan";
+
+      alang = "jpn,jp,eng,en";
+      slang = "eng,en,enUS";
+
+      deband = true;
+      deband-iterations = 2;
+      deband-threshold = 35;
+      deband-range = 20;
+      deband-grain = 5;
+
+      dither-depth = "auto";
+
+      blend-subtitles = true;
     };
-    defaultProfiles = [ "gpu-hq" ];
   };
 }
