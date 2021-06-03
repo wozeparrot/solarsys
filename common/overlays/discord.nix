8c21138dbac996271f8b459b35ff80fbbe4c9c2c
Woze Parrot <wozeparrot@gmail.com>
2021-06-03 13:43:25 -0400

    discord

--- common/overlays/discord.nix
+++ common/overlays/discord.nix
@@ -6,6 +6,7 @@ self: super:
         url = "https://discord.com/api/download/canary?platform=linux&format=tar.gz";
         sha256 = "1za7192acs46l408x6qxsa06k494sy67j1gds2f4zqnz7j81mjxv";
       };
+      nativeBuildInputs = [ super.xorg.libxshmfence ];
     }
   );
 }
