3076a40d31245fe9fd93063801348609e008f4e7
Woze Parrot <wozeparrot@gmail.com>
2021-06-03 13:47:56 -0400

    discord-canary

--- common/pkgs/default.nix
+++ common/pkgs/default.nix
@@ -2,7 +2,7 @@ self: super:
 {
   ss = {
     multimc = self.qt5.callPackage ./multimc { };
-    discord = self.callPackage ./discord { branch = "canary"; };
+    discord-canary = self.callPackage ./discord { branch = "canary"; };
     rofi = self.callPackage ./rofi { };
     picom = self.callPackage ./picom { };
     arcan = self.callPackage ./arcan { };
