160cf4c7933a3ed535deea047eddbb208cdf7929
Woze Parrot <wozeparrot@gmail.com>
2021-06-05 14:02:13 -0400

    waylandvk

--- common/components/mpv/default.nix
+++ common/components/mpv/default.nix
@@ -13,7 +13,7 @@
       osc = true;
       profile = "gpu-hq";
       gpu-api = "vulkan";
-      gpu-context = "wayland";
+      gpu-context = "waylandvk";
 
       hwdec = "auto-copy";
       hwdec-codecs = "all";
