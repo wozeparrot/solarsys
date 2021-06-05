f5eacb32bef82b29ae75ffa92a7e4dcc1abae6e3
Woze Parrot <wozeparrot@gmail.com>
2021-06-05 13:57:34 -0400

    remove thumbnail script

--- common/components/mpv/default.nix
+++ common/components/mpv/default.nix
@@ -4,13 +4,13 @@
     enable = true;
     package = pkgs.wrapMpv (pkgs.mpv-unwrapped.override { vapoursynthSupport = true; }) {
       youtubeSupport = true;
-      scripts = with pkgs.mpvScripts; [ mpris autoload thumbnail ];
+      scripts = with pkgs.mpvScripts; [ mpris autoload ];
     };
     config = {
       force-window = true;
       ytdl-format = "bestvideo[height<=?1080]+bestaudio";
       save-position-on-quit = true;
-      osc = false;
+      osc = true;
       profile = "gpu-hq";
       gpu-api = "vulkan";
       gpu-context = "wayland";
