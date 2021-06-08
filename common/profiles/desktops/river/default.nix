35b195c78e520322f425c6cb268bae03dc504e6c
Woze Parrot <wozeparrot@gmail.com>
2021-06-08 16:40:01 -0400

    river testing

--- common/profiles/desktops/river/default.nix
+++ common/profiles/desktops/river/default.nix
@@ -12,29 +12,29 @@
     river
   ];
 
-  services.greetd = {
-    settings = {
-      default_session = {
-        command =
-          let
-            hikari-run = pkgs.writeShellScriptBin "river-run" ''
-              #!/bin/sh
+  # services.greetd = {
+  #   settings = {
+  #     default_session = {
+  #       command =
+  #         let
+  #           hikari-run = pkgs.writeShellScriptBin "river-run" ''
+  #             #!/bin/sh
 
-              export XDG_SESSION_TYPE=wayland
-              export XDG_SESSION_DESKTOP=river
-              export XDG_CURRENT_DESKTOP=river
+  #             export XDG_SESSION_TYPE=wayland
+  #             export XDG_SESSION_DESKTOP=river
+  #             export XDG_CURRENT_DESKTOP=river
 
-              export MOZ_ENABLE_WAYLAND=1
-              export QT_QPA_PLATFORM=wayland-egl
-              export SDL_VIDEODRIVER=wayland
-              export _JAVA_AWT_WM_NONREPARENTING=1
+  #             export MOZ_ENABLE_WAYLAND=1
+  #             export QT_QPA_PLATFORM=wayland-egl
+  #             export SDL_VIDEODRIVER=wayland
+  #             export _JAVA_AWT_WM_NONREPARENTING=1
 
-              systemctl --user import-environment
-              systemd-cat --identifier=river dbus-run-session river
-            '';
-          in
-          "${pkgs.greetd.greetd}/bin/agreety --cmd ${river-run}/bin/river-run";
-      };
-    };
-  };
+  #             systemctl --user import-environment
+  #             systemd-cat --identifier=river dbus-run-session river
+  #           '';
+  #         in
+  #         "${pkgs.greetd.greetd}/bin/agreety --cmd ${river-run}/bin/river-run";
+  #     };
+  #   };
+  # };
 }
