6e12da51ecbe13d720691e6f31fa1d91191b082a
Woze Parrot <wozeparrot@gmail.com>
2021-06-24 14:27:02 -0400

    update flakes

--- common/components/intershell/default.nix
+++ common/components/intershell/default.nix
@@ -72,7 +72,7 @@ in
       };
       reemu = {
         body = ''
-          ffmpeg -i $argv[1] -map_metadata -1 -c:a libopus -b:a 128k -vn (basename $argv[1] $argv[2]).ogg
+          ffmpeg -i $argv[1] -map_metadata -1 -c:a libopus -b:a 128k -vn (basename $argv[1] $argv[2]).opus
           r128gain (basename $argv[1] $argv[2]).ogg
           rm $argv[1]
         '';
