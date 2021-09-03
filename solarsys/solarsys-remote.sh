60090b9df09f0bd510bd82658e6098a947da5931
Woze Parrot <wozeparrot@gmail.com>
2021-09-03 14:52:43 -0400

    more remote script work

--- solarsys/solarsys-remote.sh
+++ solarsys/solarsys-remote.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+# --- utility functions ---
+# echo to stderr
+function ercho { printf "%s\n" "$*" >&2; }
+
+# shortcut to return values
+function r { printf "%s" "$*"; }
+
+
+# --- script main ---
 
