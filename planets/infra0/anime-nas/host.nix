32c3a8e3a0002a7f4d289dbd774b74ae8181f4b3
Woze Parrot <wozeparrot@gmail.com>
2021-09-05 15:35:51 -0400

    testing wireguard

--- planets/infra0/anime-nas/host.nix
+++ planets/infra0/anime-nas/host.nix
@@ -1,9 +1,18 @@
-{ ... }: {
+{ pkgs, ... }: {
   networking.hostName = "anime-nas";
 
   imports = [
     ../common/profiles/rpi4.nix
   ];
+
+  networking.firewall.allowedUDPPorts = [ 1400 ];
+
+  networking.wg-quick.interfaces.wg0 = {
+    address = [ "fdbe:ef11:2358:1321::1/64" ];
+    listenPort = 1400;
+
+    privateKeyFile = "../../../secrets/infra0/anime-nas/wg_private";
+  };
 
   system.stateVersion = "21.11";
 }
