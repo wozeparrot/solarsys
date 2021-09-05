e40c019dfa661cd2d49534d7f7d1ceb5fd014dbf
Woze Parrot <wozeparrot@gmail.com>
2021-09-05 17:42:56 -0400

    actually add public key

--- planets/desktops/common/profiles/wireguard.nix
+++ planets/desktops/common/profiles/wireguard.nix
@@ -6,7 +6,7 @@
 
     peers = [
       {
-        publicKey = "";
+        publicKey = "W0yvMPgWIS/qKWKPg2x+7xkHNlmvJ1Ze4iFhTS1BkXk=";
         allowedIPs = [ "fdbe:ef11:2358:1321::1/128" ];
         endpoint = "192.168.0.249:5553";
         persistentKeepalive = 25;
