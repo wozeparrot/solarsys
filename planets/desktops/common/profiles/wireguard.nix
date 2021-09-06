8de87eb61259c5ede71406b156dd0ef4002da8e9
Woze Parrot <wozeparrot@gmail.com>
2021-09-05 23:13:39 -0400

    use different ip

--- planets/desktops/common/profiles/wireguard.nix
+++ planets/desktops/common/profiles/wireguard.nix
@@ -8,7 +8,7 @@
       {
         publicKey = "W0yvMPgWIS/qKWKPg2x+7xkHNlmvJ1Ze4iFhTS1BkXk=";
         allowedIPs = [ "fdbe:ef11:2358:1321::/64" ];
-        endpoint = "192.168.0.249:5553";
+        endpoint = "192.168.2.31:5553";
         persistentKeepalive = 25;
       }
     ];
