3fafd9156371da3abdbe04b649ad51d7cd48bab6
Woze Parrot <wozeparrot@gmail.com>
2021-09-05 23:13:28 -0400

    DoH

--- planets/desktops/common/profiles/network.nix
+++ planets/desktops/common/profiles/network.nix
@@ -2,11 +2,15 @@
   networking = {
     networkmanager = {
       enable = true;
-      dhcp = "dhcpcd";
-      dns = "systemd-resolved";
+      dhcp = "internal";
+      dns = "none";
       wifi.backend = "iwd";
     };
     wireless.iwd.enable = true;
+
+    dhcpcd.extraConfig = "nohook resolv.conf";
+    resolvconf.useLocalResolver = true;
+    nameservers = [ "127.0.0.1" "::1" ];
 
     firewall.enable = true;
     firewall.allowedUDPPortRanges = [
@@ -19,7 +23,23 @@
     ];
   };
 
-  services.resolved.enable = true;
+  services.resolved.enable = false;
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    settings = {
+      ipv6_servers = true;
+      require_dnssec = true;
+
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      };
+    };
+  };
 
   users.users.woze.extraGroups = [ "networkmanager" ];
 }
