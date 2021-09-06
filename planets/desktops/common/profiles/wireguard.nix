b4aca2afc4fc08505d11e675f14e222fd8ed8726
Woze Parrot <wozeparrot@gmail.com>
2021-09-06 16:04:26 -0400

    normal wireguard setup

--- planets/desktops/common/profiles/wireguard.nix
+++ planets/desktops/common/profiles/wireguard.nix
@@ -1,14 +1,13 @@
 { ... }: {
   networking.wg-quick.interfaces.wg0 = {
-    address = [ "fdbe:ef11:2358:1321::99/64" ];
-    dns = [ "fdbe:ef11:2358:1321::1" ];
+    address = [ "10.11.235.99/32" "fdbe:ef11:2358:1321::99/128" ];
     
     privateKeyFile = "/home/woze/projects/nix/solarsys/satellites/desktops/woztop/wg_private";
 
     peers = [
       {
         publicKey = "W0yvMPgWIS/qKWKPg2x+7xkHNlmvJ1Ze4iFhTS1BkXk=";
-        allowedIPs = [ "fdbe:ef11:2358:1321::/64" ];
+        allowedIPs = [ "10.11.235.0/24" "fdbe:ef11:2358:1321::/64" ];
         endpoint = "192.168.2.31:5553";
         persistentKeepalive = 25;
       }
--- planets/infra0/nas0/host.nix
+++ planets/infra0/nas0/host.nix
@@ -6,54 +6,27 @@
   ];
 
   # --- wireguard vpn setup ---
+  # set sysctl for ipv6
+  boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = 1;
+
   # open ports
   networking.firewall.allowedUDPPorts = [ 5553 ];
-  networking.firewall.interfaces.wg0 = {
-    allowedTCPPorts = [ 53 ];
-    allowedUDPPorts = [ 53 ];
-  };
-
-  # enable nat for external access
-  networking.nat = {
-    enable = true;
-    externalInterface = "eth0";
-    internalInterfaces = [ "wg0" ];
-  };
-
-  # enable dns over wireguard
-  services.dnsmasq = {
-    enable = true;
-    servers = [ "1.1.1.1" "1.0.0.1" ];
-    extraConfig = ''
-      interface=wg0
-    '';
-  };
 
   # define wireguard interface
   networking.wg-quick.interfaces.wg0 = {
-    address = [ "fdbe:ef11:2358:1321::1/64" ];
+    address = [ "10.11.235.1/24" "fdbe:ef11:2358:1321::1/64" ];
     listenPort = 5553;
 
     privateKeyFile = "/run/keys/wg_private";
 
-    postUp = ''
-      ${pkgs.iptables}/bin/ip6tables -A FORWARD -i wg0 -j ACCEPT
-      ${pkgs.iptables}/bin/ip6tables -t nat -A POSTROUTING -s fdbe:ef11:2358:1321::1/64 -o eth0 -j MASQUERADE
-    '';
-
-    preDown = ''
-      ${pkgs.iptables}/bin/ip6tables -D FORWARD -i wg0 -j ACCEPT
-      ${pkgs.iptables}/bin/ip6tables -t nat -D POSTROUTING -s fdbe:ef11:2358:1321::1/64 -o eth0 -j MASQUERADE
-    '';
-
     peers = [
       { # woztop
         publicKey = "3U2Nu7UvYIzOHPLjwKCB5iQzSNO+6hL4fTvZ+AhGHT4=";
-        allowedIPs = [ "fdbe:ef11:2358:1321::99/128" ];
+        allowedIPs = [ "10.11.235.99/32" "fdbe:ef11:2358:1321::99/128" ];
       }
       { # wone
         publicKey = "DNY6opgAbjMJh8o4O7h9dXiO4BCzg+0RM4zVNvQg3xs=";
-        allowedIPs = [ "fdbe:ef11:2358:1321::88/128" ];
+        allowedIPs = [ "10.11.235.88/32" "fdbe:ef11:2358:1321::88/128" ];
       }
     ];
   };
