053682c3b4841272531bcefacb7c00f59f7b307d
Woze Parrot <wozeparrot@gmail.com>
2021-09-06 13:56:11 -0400

    revert to systemd-resolved

--- planets/desktops/common/profiles/network.nix
+++ planets/desktops/common/profiles/network.nix
@@ -3,14 +3,12 @@
     networkmanager = {
       enable = true;
       dhcp = "internal";
-      dns = "none";
+      dns = "systemd-resolved";
       wifi.backend = "iwd";
     };
     wireless.iwd.enable = true;
 
-    dhcpcd.extraConfig = "nohook resolv.conf";
-    resolvconf.useLocalResolver = true;
-    nameservers = [ "127.0.0.1" "::1" ];
+    resolvconf.enable = false;
 
     firewall.enable = true;
     firewall.allowedUDPPortRanges = [
@@ -23,23 +21,7 @@
     ];
   };
 
-  services.resolved.enable = false;
-  services.dnscrypt-proxy2 = {
-    enable = true;
-    settings = {
-      ipv6_servers = true;
-      require_dnssec = true;
-
-      sources.public-resolvers = {
-        urls = [
-          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
-        ];
-        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
-        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
-      };
-    };
-  };
+  services.resolved.enable = true;
 
   users.users.woze.extraGroups = [ "networkmanager" ];
 }
