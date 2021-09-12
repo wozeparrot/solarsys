6197eaf94758ad056695c5cb3eb836b5eabe70ef
Woze Parrot <wozeparrot@gmail.com>
2021-09-11 23:19:20 -0400

    aninarrx

--- planets/infra0/nas0/host.nix
+++ planets/infra0/nas0/host.nix
@@ -241,12 +241,14 @@
   systemd.services."aninarr" = {
     description = "aninarr daemon";
 
+    path = with pkgs; [ bash ];
     serviceConfig = {
       ExecStart = "${pkgs.aninarr.aninarr}/bin/aninarr";
       WorkingDirectory = "/mnt/pstore1/datas/aninarr";
       Restart = "always";
       RestartSec = "5s";
       User = "root";
+      Group = "root";
     };
 
     after = [ "network.target" ];
@@ -261,6 +263,21 @@
       WorkingDirectory = "${pkgs.aninarr.aninarrl}";
       StandardOutput = "inherit";
       StandardError = "inherit";
+      Restart = "always";
+      RestartSec = "5s";
+    };
+
+    after = [ "aninarr.service" ];
+    wantedBy = [ "multi-user.target" ];
+  };
+  # aninarrx
+  systemd.services."aninarrx" = {
+    description = "aninarrx daemon"; 
+
+    path = with pkgs; [ bash jq ];
+    serviceConfig = {
+      ExecStart = "${pkgs.bash}/bin/bash helper.bash localhost no";
+      WorkingDirectory = "${pkgs.aninarr.aninarrx}";
       Restart = "always";
       RestartSec = "5s";
     };
