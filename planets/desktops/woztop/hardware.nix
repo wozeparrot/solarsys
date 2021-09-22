b67707bb97fb15e880ddc7d3c2e9bb0a95167fa6
Woze Parrot <wozeparrot@gmail.com>
2021-09-22 16:43:18 -0400

    add vfio-pci

--- planets/desktops/woztop/hardware.nix
+++ planets/desktops/woztop/hardware.nix
@@ -8,7 +8,7 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "rtsx_usb_sdmmc" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ "kvm-intel" "vfio-pci" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
