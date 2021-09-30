2685b62819fc400aef2a8e03a13fb4d3cb73cf31
Woze Parrot <wozeparrot@gmail.com>
2021-09-30 12:35:15 -0400

    [desktops.common] add uinput to user groups

--- planets/desktops/common/profiles/user.nix
+++ planets/desktops/common/profiles/user.nix
@@ -5,7 +5,7 @@
   users.users.woze = {
     isNormalUser = true;
     hashedPassword = "$6$UViRjZMnBsCT7$U6tbj1.hFnQgxnN6pm5yaF2AiXsuVVxUGpBC3kMpDLyrUXOnocdxatx.Ffmalu8IzhhSA/i2EjpvlIOgSLLJS0";
-    extraGroups = [ "wheel" "video" "input" "plugdev" "audio" "wireshark" "render" ];
+    extraGroups = [ "wheel" "video" "input" "uinput" "plugdev" "audio" "wireshark" "render" ];
     shell = pkgs.fish;
   };
 }
