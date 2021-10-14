389526713582992e1ac5f3fe02aeff7947c152e7
Woze Parrot <wozeparrot@gmail.com>
2021-10-14 19:46:52 -0400

    [flake] update aninarr

--- planets/infra0/common/profiles/rpi_aarch64.nix
+++ planets/infra0/common/profiles/rpi_aarch64.nix
@@ -27,42 +27,8 @@
 
   nixpkgs.overlays = [
     (final: prev: {
-      ripgrep = prev.rustPlatform.buildRustPackage rec {
-        pname = "ripgrep";
-        version = "13.0.0";
-
-        src = prev.fetchFromGitHub {
-          owner = "BurntSushi";
-          repo = pname;
-          rev = version;
-          sha256 = "0pdcjzfi0fclbzmmf701fdizb95iw427vy3m1svy6gdn2zwj3ldr";
-        };
-
-        cargoSha256 = "1kfdgh8dra4jxgcdb0lln5wwrimz0dpp33bq3h7jgs8ngaq2a9wp";
-
-        cargoBuildFlags = "--features pcre2";
-
-        nativeBuildInputs = with prev; [ installShellFiles pkg-config ];
-        buildInputs = with prev; [ pcre2 ];
-
-        preFixup = ''
-          installManPage $releaseDir/build/ripgrep-*/out/rg.1
-          installShellCompletion $releaseDir/build/ripgrep-*/out/rg.{bash,fish}
-          installShellCompletion --zsh complete/_rg
-        '';
-
-        doCheck = false;
-
-        doInstallCheck = true;
-        installCheckPhase = ''
-          file="$(mktemp)"
-          echo "abc\nbcd\ncde" > "$file"
-          $out/bin/rg -N 'bcd' "$file"
-          $out/bin/rg -N 'cd' "$file"
-
-          echo '(a(aa)aa)' | $out/bin/rg -P '\((a*|(?R))*\)'
-        '';
-      };
+      ripgrep = prev.mpkgs.ripgrep;
+      neovim = prev.mpkgs.neovim;
       pango = prev.pango.overrideAttrs (oldAttrs: {
         outputs = [ "bin" "out" "dev" ];
         mesonFlags = [ "-Dintrospection=disabled" "-Dgtk_doc=false" ];
