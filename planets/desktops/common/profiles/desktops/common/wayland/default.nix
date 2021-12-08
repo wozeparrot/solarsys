0e48eb86a1b248b9ca464777214b2bd88dc04bd4
Woze Parrot <wozeparrot@gmail.com>
2021-12-07 20:07:27 -0500

    [desktops.common] waybar show hour on mpd time

--- planets/desktops/common/profiles/desktops/common/wayland/default.nix
+++ planets/desktops/common/profiles/desktops/common/wayland/default.nix
@@ -37,7 +37,7 @@
           modules-right = [ "backlight" "pulseaudio" "clock" "tray" ];
           modules = {
             "mpd" = {
-              format = "{stateIcon} : {title} : ({elapsedTime:%M:%S}/{totalTime:%H:%M:%S})";
+              format = "{stateIcon} : {title} : ({elapsedTime::%H:%M:%S}/{totalTime:%H:%M:%S})";
               format-disconnected = "disconnected";
               format-stopped = "stopped";
               state-icons = {
