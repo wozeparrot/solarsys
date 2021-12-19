9e989261258ad7b6e043e3b6f25f3b20386c2824
Woze Parrot <wozeparrot@gmail.com>
2021-12-19 15:50:53 -0500

    [common] fix discord build

--- common/pkgs/default.nix
+++ common/pkgs/default.nix
@@ -1,7 +1,7 @@
 self: super:
 {
   ss = {
-    discord-canary = self.callPackage ./discord { branch = "canary"; useWayland = true; };
+    discord-canary = self.callPackage ./discord { branch = "canary"; useWayland = false; };
     rivercarro = self.callPackage ./rivercarro { };
     river = self.callPackage ./river { };
     shotcut = self.libsForQt5.callPackage ./shotcut { };
