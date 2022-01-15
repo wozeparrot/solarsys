ce097a3222e4e1c1d4493d976c8a77808eecf5ec
Woze Parrot <wozeparrot@gmail.com>
2022-01-15 13:12:13 -0500

    [desktops.common] git ssh signing

--- planets/desktops/common/components/git/default.nix
+++ planets/desktops/common/components/git/default.nix
@@ -44,12 +44,9 @@
 
       core.editor = "nvim";
 
-      #gpg.format = "ssh";
-      #user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKPL+OWmcGo4IlL+LUz9uEgOH8hk0JIN3DXEV8sdgxPB wozeparrot";
-      #gpg.ssh.allowedSignersFile = "~/.local/share/git_allowed_signers";
+      gpg.format = "ssh";
+      user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKPL+OWmcGo4IlL+LUz9uEgOH8hk0JIN3DXEV8sdgxPB wozeparrot";
+      gpg.ssh.allowedSignersFile = "~/.local/share/git_allowed_signers";
     };
   };
-  xdg.dataFile.git_allowed_signers.text = ''
-    wozeparrot@gmail.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKPL+OWmcGo4IlL+LUz9uEgOH8hk0JIN3DXEV8sdgxPB wozeparrot
-  '';
 }
