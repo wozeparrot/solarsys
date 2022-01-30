60f6229f9496ff271184402e2b5fd78c1f2da9dc
Woze Parrot <wozeparrot@gmail.com>
2022-01-30 15:32:25 -0500

    [common] custom matrix-conduit from git

--- common/pkgs/default.nix
+++ common/pkgs/default.nix
@@ -5,6 +5,8 @@ self: super:
     rivercarro = self.callPackage ./rivercarro { };
     river = self.callPackage ./river { };
     shotcut = self.libsForQt5.callPackage ./shotcut { };
+
+    matrix-conduit = self.callPackage ./matrix-conduit { };
   };
 
   yt-dlp = self.python3Packages.callPackage ./yt-dlp { };
---
+++ common/pkgs/matrix-conduit/default.nix
@@ -0,0 +1,22 @@
+{ stdenv, lib, fetchFromGitLab, rustPlatform, }:
+
+rustPlatform.buildRustPackage rec {
+  pname = "matrix-conduit";
+  version = "fb2a7ebf66cd358b391eb6c25524fccf35f138dd";
+
+  src = fetchFromGitLab {
+    owner = "famedly";
+    repo = "conduit";
+    rev = version;
+    sha256 = "sha256-C10W75MNZdkaJDlXqYoEejI0UvOvswqVaZqvn4xZUxU=";
+  };
+
+  cargoSha256 = "sha256-zyzgxNBUO+xR/kL3CQHI3gCE0wS7H//ujUvCtrs69tc=";
+
+  meta = with lib; {
+    description = "A Matrix homeserver written in Rust";
+    homepage = "https://conduit.rs/";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ pstn piegames ];
+  };
+}
