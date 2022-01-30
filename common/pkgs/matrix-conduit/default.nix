1e99d64d3497da276846d2eb6196c441a29b8a6e
Woze Parrot <wozeparrot@gmail.com>
2022-01-30 17:38:33 -0500

    [common] update build features

--- common/pkgs/matrix-conduit/default.nix
+++ common/pkgs/matrix-conduit/default.nix
@@ -13,6 +13,9 @@ rustPlatform.buildRustPackage rec {
 
   cargoSha256 = "sha256-zyzgxNBUO+xR/kL3CQHI3gCE0wS7H//ujUvCtrs69tc=";
 
+  buildNoDefaultFeatures = true;
+  buildFeatures = [ "conduit_bin" "backend_sqlite" ];
+
   meta = with lib; {
     description = "A Matrix homeserver written in Rust";
     homepage = "https://conduit.rs/";
