35d7375c8e61d286ba12b0eeae8866616e1cfee0
Woze Parrot <wozeparrot@gmail.com>
2022-03-05 10:49:04 -0500

    [desktops.common] correct profile dir

--- planets/desktops/common/components/musicprod/default.nix
+++ planets/desktops/common/components/musicprod/default.nix
@@ -36,11 +36,11 @@
   ];
 
   home.sessionVariables = {
-    DSSI_PATH = "$HOME/.dssi:$HOME/.nix-profile/lib/dssi:/run/current-system/sw/lib/dssi";
-    LADSPA_PATH = "$HOME/.ladspa:$HOME/.nix-profile/lib/ladspa:/run/current-system/sw/lib/ladspa";
-    LV2_PATH = "$HOME/.lv2:$HOME/.nix-profile/lib/lv2:/run/current-system/sw/lib/lv2";
-    LXVST_PATH = "$HOME/.lxvst:$HOME/.nix-profile/lib/lxvst:/run/current-system/sw/lib/lxvst";
-    VST_PATH = "$HOME/.vst:$HOME/.nix-profile/lib/vst:/run/current-system/sw/lib/vst";
-    VST3_PATH = "$HOME/.vst3:$HOME/.nix-profile/lib/vst3:/run/current-system/sw/lib/vst3";
+    DSSI_PATH = "$HOME/.dssi:/nix/var/nix/profiles/per-user/woze/home-manager/home-path/lib/dssi:/run/current-system/sw/lib/dssi";
+    LADSPA_PATH = "$HOME/.ladspa:/nix/var/nix/profiles/per-user/woze/home-manager/home-path/lib/ladspa:/run/current-system/sw/lib/ladspa";
+    LV2_PATH = "$HOME/.lv2:/nix/var/nix/profiles/per-user/woze/home-manager/home-path/lib/lv2:/run/current-system/sw/lib/lv2";
+    LXVST_PATH = "$HOME/.lxvst:/nix/var/nix/profiles/per-user/woze/home-manager/home-path/lib/lxvst:/run/current-system/sw/lib/lxvst";
+    VST_PATH = "$HOME/.vst:/nix/var/nix/profiles/per-user/woze/home-manager/home-path/lib/vst:/run/current-system/sw/lib/vst";
+    VST3_PATH = "$HOME/.vst3:/nix/var/nix/profiles/per-user/woze/home-manager/home-path/lib/vst3:/run/current-system/sw/lib/vst3";
   };
 }
