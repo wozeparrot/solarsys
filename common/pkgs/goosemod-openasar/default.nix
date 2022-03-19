824eb6e4738dc24739c515da370c4cfecf251c0d
Woze Parrot <wozeparrot@gmail.com>
2022-03-19 00:17:40 -0400

    [common.pkgs] update goosemod-openasar

--- common/pkgs/goosemod-openasar/default.nix
+++ common/pkgs/goosemod-openasar/default.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation rec {
   pname = "goosemod-openasar";
-  version = "d7d372e068001d10f573540e4872ef0c538ebd23";
+  version = "82037247e0c5e9509a5b4dc7dbd4894fdbb02697";
 
   src = fetchFromGitHub {
     owner = "GooseMod";
     repo = "OpenAsar";
     rev = version;
-    sha256 = "sha256-K0xV4gM/dEqPZAltskZ8KBiZamf9YsO9gxXu5ltwhPE=";
+    sha256 = "sha256-yl0azG2Od6KOf4y2aLOQDRFK/79MWU9EVOpXxi2VdCE=";
   };
 
   nativeBuildInputs = [
