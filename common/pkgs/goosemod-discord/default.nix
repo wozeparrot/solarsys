021ca4952b5e566a9933f73de8891b1b616399f6
Woze Parrot <wozeparrot@gmail.com>
2022-09-14 09:59:00 -0400

    feat: update discord

--- common/pkgs/goosemod-discord/default.nix
+++ common/pkgs/goosemod-discord/default.nix
@@ -8,10 +8,10 @@ in {
     pname = "discord";
     binaryName = "Discord";
     desktopName = "Discord GM";
-    version = "0.0.18";
+    version = "0.0.19";
     src = fetchurl {
       url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
-      sha256 = "1hl01rf3l6kblx5v7rwnwms30iz8zw6dwlkjsx2f1iipljgkh5q4";
+      sha256 = "1403vdc7p6a8mhr114brfp4dqvikaj5s71wrx20ca5y6srsv5x0r";
     };
   };
   ptb = callPackage ./base.nix rec {
@@ -19,10 +19,10 @@ in {
     pname = "discord-ptb";
     binaryName = "DiscordPTB";
     desktopName = "Discord PTB GM";
-    version = "0.0.31";
+    version = "0.0.33";
     src = fetchurl {
       url = "https://dl-ptb.discordapp.net/apps/linux/${version}/discord-ptb-${version}.tar.gz";
-      sha256 = "04yvk7d4p1g82xnv4y0mqq29303xm4mrx0zmjqszk4ayq9fdy02x";
+      sha256 = "0887ncnbiab3h17bbgqz8mjjrgg6fcdkwpi1vcf1ghl6bfa9dsrp";
     };
   };
   canary = callPackage ./base.nix rec {
@@ -30,10 +30,10 @@ in {
     pname = "discord-canary";
     binaryName = "DiscordCanary";
     desktopName = "Discord Canary GM";
-    version = "0.0.136";
+    version = "0.0.139";
     src = fetchurl {
       url = "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
-      sha256 = "01a855g3bj989ydd304ipvpjmz1p8ha4f3hl0q3yp2gk6pia1c9s";
+      sha256 = "1llafigs2zd49m212d49zi4klx39kn4r2msxz75cmi4i6p8wxxzw";
     };
   };
 }.${branch}
