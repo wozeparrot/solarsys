7e638044a44682af59f8acb9026613f7a3250ae9
Woze Parrot <wozeparrot@gmail.com>
2022-09-26 01:02:56 -0400

    feat: overhaul tlp for woztop-horizon

--- planets/desktops/common/profiles/laptop.nix
+++ planets/desktops/common/profiles/laptop.nix
@@ -36,19 +36,37 @@
   # power management
   services.tlp.enable = true;
   services.tlp.settings = {
-    CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-    CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+    CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
+    CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
 
-    CPU_SCALING_GOVERNOR_ON_AC = "performance";
-    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    CPU_SCALING_MIN_FREQ_ON_AC = "400000";
+    CPU_SCALING_MAX_FREQ_ON_AC = "4935000";
+    CPU_SCALING_MIN_FREQ_ON_BAT = "400000";
+    CPU_SCALING_MAX_FREQ_ON_BAT = "2400000";
 
-    CPU_BOOST_ON_AC = 1;
-    CPU_BOOST_ON_BAT = 0;
+    CPU_BOOST_ON_AC = "1";
+    CPU_BOOST_ON_BAT = "0";
 
-    CPU_MIN_PERF_ON_AC = 0;
-    CPU_MIN_PERF_ON_BAT = 0;
-    CPU_MAX_PERF_ON_AC = 100;
-    CPU_MAX_PERF_ON_BAT = 30;
+    SCHED_POWERSAVE_ON_AC = "0";
+    SCHED_POWERSAVE_ON_BAT = "1";
+
+    NMI_WATCHDOG = "0";
+
+    PCIE_ASPM_ON_AC = "default";
+    PCIE_ASPM_ON_BAT = "powersupersave";
+
+    RADEON_DPM_PERF_LEVEL_ON_AC = "auto";
+    RADEON_DPM_PERF_LEVEL_ON_BAT = "low";
+    RADEON_DPM_STATE_ON_AC = "performance";
+    RADEON_DPM_STATE_ON_BAT = "battery";
+
+    WOL_DISABLE = "Y";
+
+    START_CHARGE_THRESH_BAT0 = "0";
+    STOP_CHARGE_THRESH_BAT0 = "80";
+
+    PLATFORM_PROFILE_ON_AC = "performance";
+    PLATFORM_PROFILE_ON_BAT = "quiet";
   };
   services.logind.lidSwitch = "suspend";
 }
