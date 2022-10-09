73d80561bd1619ccd1deef71366f7debcf9cdb21
Woze Parrot <wozeparrot@gmail.com>
2022-10-09 17:49:58 -0400

    feat: gamescope wrapper

--- planets/desktops/woztop-horizon/home.nix
+++ planets/desktops/woztop-horizon/home.nix
@@ -20,6 +20,14 @@
         DRI_PRIME=1 exec -a "$0" "$@"
       ''
     )
+
+    (
+      pkgs.writeShellScriptBin "run_gamescope" ''
+        #!/usr/bin/env bash
+
+        exec gamescope --prefer-vk-device 1002:1681 -f -U -- "$@"
+      ''
+    )
   ];
 
   home.stateVersion = "22.11";
