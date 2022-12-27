dab2278768220228dcacb443b972a5da8a3a8ace
Woze Parrot <wozeparrot@gmail.com>
2022-12-27 08:53:34 -0500

    flake: remove hyprland cachix

--- flake.nix
+++ flake.nix
@@ -114,10 +114,6 @@
       };
       hyprland = {
         inherit (inputs.hyprland) packages;
-        cache = {
-          substituters = ["https://hyprland.cachix.org"];
-          trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-        };
       };
       hyprpicker = {
         inherit (inputs.hyprpicker) packages;
