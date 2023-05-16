af8ce208abdb8148cd983661c417e37cd54d9aed
Woze Parrot <wozeparrot@gmail.com>
2023-05-15 22:04:51 -0400

    taurus/wangshu: reset to base

--- planets/taurus/wangshu/host.nix
+++ planets/taurus/wangshu/host.nix
@@ -4,6 +4,7 @@
   imports = [
     ./hardware.nix
     ../common/profiles/base.nix
+    ../common/profiles/vpn.nix
   ];
 
   # important config
@@ -30,80 +31,19 @@
   # --- open ports ---
   networking.firewall = {
     allowedUDPPorts = [
-      # minecraft server
-      25565
     ];
     allowedTCPPorts = [
-      # minecraft server
-      25565
     ];
-    interfaces.wg0 = {
+    interfaces.orion = {
       allowedUDPPorts = [
-        # minecraft server
-        25565
       ];
       allowedTCPPorts = [
-        # minecraft server
-        25565
-
-        # seaweedfs
-        9301
-        19301
-        9302
-        19302
-        9311
-        19311
       ];
     };
   };
 
   # --- packages ---
   environment.systemPackages = with pkgs; [];
-
-  # --- wireguard vpn setup ---
-  networking.wg-quick.interfaces = {
-    wg0 = {
-      address = ["10.11.235.11/24" "fdbe:ef11:2358:1321::11/64"];
-      dns = ["10.11.235.1" "fdbe:ef11:2358:1321::1"];
-
-      privateKeyFile = "/keys/wg_private";
-
-      peers = [
-        {
-          publicKey = "W0yvMPgWIS/qKWKPg2x+7xkHNlmvJ1Ze4iFhTS1BkXk=";
-          allowedIPs = ["10.11.235.0/24" "fdbe:ef11:2358:1321::/64"];
-          endpoint = "192.168.2.31:5553";
-          persistentKeepalive = 25;
-        }
-      ];
-    };
-  };
-
-  # --- containers ---
-  containers.seaweedfs-node = {
-    autoStart = true;
-    bindMounts."/var/lib/seaweedfs/data" = {
-      hostPath = "/opt/deepspace";
-      isReadOnly = false;
-    };
-    config = {config, ...}: {
-      # -- seaweedfs volume --
-      systemd.services."seaweedfs-volume" = {
-        description = "seaweedfs volume server";
-
-        serviceConfig = {
-          ExecStart = "${pkgs.seaweedfs}/bin/weed volume -ip 10.11.235.11 -port 9311 -mserver '10.11.235.1:9301' -index leveldb -max 12 -dir /var/lib/seaweedfs/data/";
-          Restart = "always";
-          RestartSec = "10s";
-        };
-
-        after = ["network.target"];
-        wantedBy = ["multi-user.target"];
-      };
-
-      system.stateVersion = "22.11";
-    };
-  };
 
   system.stateVersion = "21.11";
 }
