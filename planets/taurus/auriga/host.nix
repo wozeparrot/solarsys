a329661260f98fd815369943f890e6046c6d27d6
Woze Parrot <wozeparrot@gmail.com>
2023-06-12 20:37:20 -0400

    taurus/auriga: seaweedfs init

--- planets/taurus/auriga/host.nix
+++ planets/taurus/auriga/host.nix
@@ -8,7 +8,16 @@
   imports = [
     ../common/profiles/rpi4.nix
     ../common/profiles/vpn.nix
+    ../common/containered-services/seaweedfs-node.nix
   ];
+
+  # --- mount disks ---
+  fileSystems = {
+    "/mnt/pstore0" = {
+      device = "/dev/disk/by-uuid/b26f275b-b03d-4895-89e2-c986cab78a00";
+      fsType = "xfs";
+    };
+  };
 
   # --- open ports ---
   networking.firewall = {
@@ -31,6 +40,15 @@
   services.udev.extraRules = ''
     SUBSYSTEM=="usb", TEST=="power/autosuspend" ATTR{power/autosuspend}="-1"
   '';
+
+  # --- seaweedfs ---
+  containered-services.seaweedfs-node = {
+    enable = true;
+    bindAddress = "10.11.235.22";
+    volumes = [
+      "/mnt/pstore0/seaweedfs/volume"
+    ];
+  };
 
   system.stateVersion = "23.05";
 }
