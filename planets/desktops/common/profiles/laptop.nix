41e8fc43d9fc8fd09c0c330de9be280b17ac2d9f
Woze Parrot <wozeparrot@gmail.com>
2024-03-23 19:21:44 -0400

    desktops/profiles: laptop: perf policy

--- planets/desktops/common/profiles/laptop.nix
+++ planets/desktops/common/profiles/laptop.nix
@@ -26,15 +26,15 @@
     tlp = {
       enable = true;
       settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_AC = "balanced";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
         CPU_SCALING_MIN_FREQ_ON_AC = "400000";
         CPU_SCALING_MAX_FREQ_ON_AC = "4935000";
         CPU_SCALING_MIN_FREQ_ON_BAT = "400000";
-        CPU_SCALING_MAX_FREQ_ON_BAT = "1800000";
+        CPU_SCALING_MAX_FREQ_ON_BAT = "3600000";
 
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balanced";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
         CPU_BOOST_ON_AC = "1";
