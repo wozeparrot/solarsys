1a0977939f75403e86db0eb92e33c3164a501761
Woze Parrot <wozeparrot@gmail.com>
2024-08-04 14:28:04 -0700

    solarsys: apply extra opts here as well

--- solarsys/common.bash
+++ solarsys/common.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-FLK="$(nix eval .#planets --json --apply "(builtins.getFlake \"git+file://\${toString ./.}?rev=$(git rev-parse HEAD)\").lib.attrsets.filterAttrsRecursive (n: v: n != \"core\")")"
+FLK="$(nix $NIX_EXTRA_OPTS eval .#planets --json --apply "(builtins.getFlake \"git+file://\${toString ./.}?rev=$(git rev-parse HEAD)\").lib.attrsets.filterAttrsRecursive (n: v: n != \"core\")")"
 
 # gets planets from flake
 function get_planets {
