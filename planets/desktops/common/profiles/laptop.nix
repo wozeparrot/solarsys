50f1ce25c0293b75d2e9639b5d7f5ec22891591f
Woze Parrot <wozeparrot@gmail.com>
2026-01-13 14:42:52 -0800

    desktops/laptop: switch to tuned

--- planets/desktops/common/profiles/laptop.nix
+++ planets/desktops/common/profiles/laptop.nix
@@ -28,52 +28,10 @@
 
   # power management
   boot.kernelParams = [ "rcutree.enable_rcu_lazy=1" ];
-  services = {
-    tlp = {
-      enable = lib.mkDefault true;
-      settings = {
-        CPU_DRIVER_OPMODE_ON_AC = "active";
-        CPU_DRIVER_OPMODE_ON_BAT = "active";
-
-        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-        CPU_SCALING_MIN_FREQ_ON_AC = "400000";
-        CPU_SCALING_MAX_FREQ_ON_AC = "4935000";
-        CPU_SCALING_MIN_FREQ_ON_BAT = "400000";
-        CPU_SCALING_MAX_FREQ_ON_BAT = "3000000";
-
-        CPU_MIN_PERF_ON_AC = "0";
-        CPU_MAX_PERF_ON_AC = "100";
-        CPU_MIN_PERF_ON_BAT = "0";
-        CPU_MAX_PERF_ON_BAT = "70";
-
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-
-        CPU_BOOST_ON_AC = "1";
-        CPU_BOOST_ON_BAT = "0";
-
-        NMI_WATCHDOG = "0";
-
-        RADEON_DPM_PERF_LEVEL_ON_AC = "auto";
-        RADEON_DPM_PERF_LEVEL_ON_BAT = "low";
-        RADEON_DPM_STATE_ON_AC = "performance";
-        RADEON_DPM_STATE_ON_BAT = "battery";
-
-        RUNTIME_PM_ON_AC = "on";
-        RUNTIME_PM_ON_BAT = "auto";
-        PCIE_ASPM_ON_AC = "default";
-        PCIE_ASPM_ON_BAT = "powersupersave";
-
-        WOL_DISABLE = "Y";
-
-        START_CHARGE_THRESH_BAT0 = "0";
-        STOP_CHARGE_THRESH_BAT0 = "60";
-
-        PLATFORM_PROFILE_ON_AC = "balanced";
-        PLATFORM_PROFILE_ON_BAT = "quiet";
-      };
+  services.tuned = {
+    enable = lib.mkDefault true;
+    settings = {
+      dynamic_tuning = true;
     };
   };
 
