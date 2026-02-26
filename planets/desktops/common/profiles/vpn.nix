465ef2b6441dda661c9737781e61190b60acba21
Woze Parrot <wozeparrot@gmail.com>
2026-02-26 12:39:49 +0800

    desktops/common: vpn: use full mullvad package

--- planets/desktops/common/profiles/vpn.nix
+++ planets/desktops/common/profiles/vpn.nix
@@ -2,11 +2,10 @@
 {
   environment.systemPackages = with pkgs; [
     wireguard-tools
-    master.mullvad
   ];
 
   networking.firewall.checkReversePath = "loose";
 
   services.mullvad-vpn.enable = true;
-  services.mullvad-vpn.package = pkgs.master.mullvad;
+  services.mullvad-vpn.package = pkgs.mullvad-vpn;
 }
